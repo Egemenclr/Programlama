5c8b5b74b1d2ac9060f6b590948c9085efc718d2
Egemenclr <45071098+Egemenclr@users.noreply.github.com>
2019-03-11 12:50:25 +0300

    Update frekans.py

--- frekans.py
+++ frekans.py
@@ -1,35 +1,31 @@
-def frequency (list) :
-    freq_table = {}
+def frekans (list) :
+    frekans_table = {}
     for item in list :
         frekans = 0
         for item2 in list :
             if item == item2:
                 frekans += 1
-        #freq_table.update ({item:frequency})
-        freq_table[item]=frekans
-    return freq_table
-
-
-
-
+        #frekans_table.update ({item:frequency})
+        frekans_table[item]=frekans
+    return frekans_table
 
 def frequency2(list):
-    freq_dict = {}
+    frekans_dict = {}
     for item in list :
-        if (item in freq_dict):
-            freq_dict[item] = freq_dict[item]+1
+        if (item in frekans_dict):
+            frekans_dict[item] = frekans_dict[item]+1
         else:
-            freq_dict[item] = 1
-    return freq_dict
+            frekans_dict[item] = 1
+    return frekans_dict
 
 def frequency_with_list(list):
-    frequency_list = []
+    frekans_list = []
     for i in range(len(list)):
         s = False
-        for j in range(len(frequency_list)):
-            if list[i] == frequency_list[j][0]:
-                frequency_list[j][1] += 1
+        for j in range(len(frekans_list)):
+            if list[i] == frekans_list[j][0]:
+                frekans_list[j][1] += 1
                 s = True
         if not s:
-            frequency_list.append([list[i],1])
-    return frequency_list
+            frekans_list.append([list[i],1])
+    return frekans_list
