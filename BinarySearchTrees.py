fca3cd9f971e99c0c58578af4871ac5b494ea84d
Egemen Inceler <45071098+Egemenclr@users.noreply.github.com>
2019-03-24 21:44:29 +0300

    Update BinarySearchTrees.py

--- BinarySearchTrees.py
+++ BinarySearchTrees.py
@@ -5,61 +5,63 @@
 
 
 import random
-class myNode(object):
+class Node(object):
     def __init__(self,v=0):
         self.value=v
         self.left=None
         self.right=None
         
-class myTree(object):
-        def __init__(self):
-            self.root=myNode(50)
-            
-            
+class Tree(object):
+    def __init__(self):
+        self.root=Node()
+        
+        
+def insert(root,node):
+    if root:
+        if(root.value < node.value):
+            if root.right:
+                insert(root.right,node)
+            else:
+                root.right=node
+        else:
+            if root.left:
+                insert(root.left,node)
+            else:
+                root.left=node
+        
+    else:
+        root=node
+
+
+                
 def inorder(root):
     if root:
         inorder(root.left)
         print(root.value, end=" ")
         inorder(root.right)
+def preorder(root):
+    if root:
+        print(root.value,end=" ")
+        preorder(root.left)
+        preorder(root.right)
     
-def insert(root,node):
-    if root is None:
-        root=node
-    else:
-        if(root.value < node.value):
-            if root.right is None:
-                root.right=node
-            else:
-                insert(root.right,node)
-        else:
-            if(root.left is None):
-                root.left=node
-            else:
-                insert(root.left,node)
-                
+def postorder(root):
+    if root:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.value,end=" ")
+        
 def test():
     numbers=[]
     for x in range(5):
         numbers.append(random.randint(1,101))
-    tree1=myTree()
+    tree1=Tree()
     
     for k in numbers:
-        insert(tree1.root,myNode(k))
+        insert(tree1.root,Node(k))
     inorder(tree1.root)
-test()
-
-
-# In[64]:
-
-
-m1=myNode()
-m2=myNode(60)
-#m3=myNode(20)
-t1=myTree()
-
-insert(t1.root,m1)
-insert(t1.root,m2)
-insert(t1.root,m3)
-#t1.root.right.value
-inorder(t1.root)
+    print()
+    preorder(tree1.root)
+    print()
+    postorder(tree1.root)
 
