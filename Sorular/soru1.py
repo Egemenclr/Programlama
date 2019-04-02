94c854589ba2305e6dd334b8dbb5a97b1d007485
Egemen Inceler <45071098+Egemenclr@users.noreply.github.com>
2019-04-03 01:00:52 +0300

    Add files via upload

---
+++ Sorular/soru1.py
@@ -0,0 +1,34 @@
+
+# coding: utf-8
+
+def fonk(text):
+    mySentences=text.split(".")
+    myWords=[]
+    for cumle in mySentences:
+        kelimeler=cumle.split(" ")
+        for kelime in kelimeler:
+            myWords.append(kelime)
+            
+    return myWords
+def var_mi(myWords,string):
+    
+    for i in myWords:
+        if(string in myWords):
+            return myWords.index(string),True
+        else:
+            return False
+def index_listesi(myWords,string):
+    my_list=[]
+    for i in range(len(myWords)):
+        
+        if(string ==myWords[i]):
+            my_list.append(i)
+          
+    return my_list
+
+text1="Do you egemen know about egemen their egemen art project?” asked Amy. 'It’s about graffiti, I think,' said Tara. “They’re working on egemen it at the old house behind the egemen factory."
+kelimeler=fonk(text1)
+#kelimeler
+#var_mi(kelimeler,"know")
+string_listesi(kelimeler,"egemen")
+
